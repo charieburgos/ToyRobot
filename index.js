3641432add224eb8d190035a38963a7a20a8e1f1
Charie <charie.b@vendorpanel.com>
2025-01-14 18:12:03 +1100

    Redo of robot challenge after Section 9.

--- index.js
+++ index.js
@@ -1,141 +1,79 @@
 "use strict";
 
+let newX = 0;
+let newY = 0;
+let newF = "SOUTH WEST";
+
 //dimensions of the table top; default is 5 x 5.
 const xLimit = 5;
 const yLimit = 5;
 
-//valid moves
-const validF = ["North", "South", "East", "West"];
+const output = (command, x, y, f) =>
+  console.log(`Robot ${command} at coordinate (${x}, ${y}) facing ${f}.`);
 
-//starting point;
-let x = 0;
-let y = 0;
-let f = "SOUTH WEST";
+//Creates input field on webpage and button to submit input
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
 
-//get input and split into array
-let instructions = [];
-const input = prompt("Where should the robot go?");
-instructions = input.split(" ");
+//Manipulate input from user and calls respective functions
+document.querySelector("button").addEventListener("click", function () {
+  const instructions = document.querySelector("textarea").value;
+  const instructionsSplit = instructions.split("\n");
 
-//moves the robot one unit forward in the direction it is currently facing.
-//north = (0, 1);
-//south = (0, -1);
-//east = (1, 0);
-//west = (-1, 0);
+  for (const [i, row] of instructionsSplit.entries()) {
+    const withinBoard = newX <= xLimit && newY <= yLimit;
+
+    if (withinBoard) {
+      console.log(`Instruction ${i + 1}: ${row}`);
+    } else {
+      console.log("Robot has fallen off the board :(");
+    }
+
+    if (row.includes("PLACE")) {
+      const [command, coordinates] = row.split(" ");
+      const [x, y, f] = coordinates.split(",");
+      output("placed", x, y, f);
+      [newX, newY, newF] = [Number(x), Number(y), f];
+    }
+
+    if (row.includes("MOVE") && withinBoard) move(newX, newY, newF);
+    if (row.includes("LEFT") && withinBoard) turnLeft(newX, newY, newF);
+    if (row.includes("RIGHT") && withinBoard) turnRight(newX, newY, newF);
+    if (row.includes("REPORT") && withinBoard) report(newX, newY, newF);
+  }
+});
+
+//List of valid directions
+const validF = ["NORTH", "EAST", "SOUTH", "WEST"];
+
+//Functions to rotate robots
+const turnLeft = function (x, y, f) {
+  newF = validF[validF.indexOf(f) - 1] || "WEST";
+  output("rotated to the left", x, y, newF);
+};
+
+const turnRight = function (x, y, f) {
+  newF = validF[validF.indexOf(f) + 1] || "WEST";
+  output("rotated to the right", x, y, newF);
+};
+
+//Function to moves the robot one unit forward in the direction it is currently facing.
+const moveY = function (x, y, f, p) {
+  newY = y + p;
+  output("moved", x, newY, f);
+};
+
+const moveX = function (x, y, f, p) {
+  newX = x + p;
+  output("moved", x, newY, f);
+};
 
 const move = function (x, y, f) {
-  if (f == "NORTH") {
-    x += 0;
-    y += 1;
-  } else if (f == "EAST") {
-    x += 1;
-    y += 0;
-  } else if (f == "SOUTH") {
-    x += 0;
-    y -= 1;
-  } else if (f == "WEST") {
-    x -= 1;
-    y += 0;
-  } else {
-    x, y, f;
-  }
-  console.log(`Robot moved to ${x}, ${y} facing ${f}`);
-};
-
-//rotates robot 90 degrees to the left.
-const rotateLeft = function (x, y, f) {
-  switch (f) {
-    case "NORTH":
-      f = "WEST";
-      break;
-    case "WEST":
-      f = "SOUTH";
-      break;
-    case "SOUTH":
-      f = "EAST";
-      break;
-    case "EAST":
-      f = "NORTH";
-  }
-  return x, y, f;
-};
-
-//rotates robot 90 degrees to the right.
-const rotateRight = function (x, y, f) {
-  switch (f) {
-    case "NORTH":
-      console.log("EAST");
-      break;
-    case "EAST":
-      console.log("SOUTH");
-      break;
-    case "SOUTH":
-      console.log("WEST");
-      break;
-    case "WEST":
-      console.log("NORTH");
-    default:
-      console.log(f);
-  }
+  if (f === "NORTH") moveY(x, y, f, 1);
+  if (f === "EAST") moveX(x, y, f, 1);
+  if (f === "SOUTH") moveY(x, y, f, -1);
+  if (f === "WEST") moveX(x, y, f, -1);
 };
 
 //reports the coordinates of robot
-const report = function (posX, posY, posF) {
-  console.log(`Robot is at position: ${posX}, ${posY}, ${posF}.`);
-};
-
-//check if PLACE is provided if so, get the instructions after.
-//Currently not providing newline so need to do some weirdness
-
-// const placeRobot = function (instructions) {
-//   const placePos = instructions.indexOf("PLACE");
-//   if (placePos >= 0) {
-//     const placeCoords = instructions[placePos + 1];
-//     [x, y, f] = placeCoords.split(",");
-//     getCoordinates(x, y, f);
-//   }
-// };
-
-// //place: adds the robot on the table top where x is the x coordinate and y is the y coordinate and f where the robot is facing.
-const getCoordinates = function (coords) {
-  //place the robot's initial position
-  const placePos = coords.indexOf("PLACE");
-
-  if (placePos >= 0) {
-    const placeCoords = coords[placePos + 1];
-    [x, y, f] = placeCoords.split(",");
-    x = Number(x);
-    y = Number(y);
-  }
-
-  const moves = [...coords.splice(placePos + 2)];
-
-  // let i = 0;
-  // while (i < moves.length) {
-  //   i++;
-  //   if (moves[i] == "MOVE") move(x, y, f);
-  //   if (moves[i] == "LEFT") rotateLeft(x, y, f);
-  //   if (moves[i] == "RIGHT") rotateRight(x, y, f);
-  //   if (moves[i] == "REPORT") report(x, y, f);
-  // }
-
-  for (let i = 0; i < moves.length; i++) {
-    if (moves[i] == "MOVE") {
-      move(x, y, f);
-    } else if (moves[i] == "LEFT") {
-      rotateLeft(x, y, f);
-    } else if (moves[i] == "RIGHT") {
-      rotateRight(x, y, f);
-    } else report(x, y, f);
-  }
-};
-
-//Test case 1: PLACE 0,0,NORTH MOVE REPORT
-
-//getCoordinates(instructions);
-
-//getCoordinates(instructions);
-//const coords = "0,0,NORTH";
-//console.log(move(0, 0, "NORTH"));
-
-getCoordinates(instructions);
+const report = (x, y, f) => output("is", x, newY, newF);
