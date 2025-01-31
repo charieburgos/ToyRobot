526facb69faf33a00a21ce060ae809993994a391
Charie <charie.b@vendorpanel.com>
2025-01-31 17:57:05 +1100

    Refining Commands class to encapsulate instructions on how the toy should move rather than writing it on Toy class itself.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 const directions = ["NORTH", "EAST", "SOUTH", "WEST"];
 
+//Class to define the table top dimensions
 class Table {
   constructor(xOrigin, yOrigin, xLimit, yLimit) {
     this.xOrigin = xOrigin;
@@ -10,25 +11,34 @@ class Table {
   }
 }
 
+//Class to define instructions on how commands work
 class Commands {
   move() {
-    this.moveDirections = {
+    return {
       NORTH: [0, 1],
       EAST: [1, 0],
       SOUTH: [0, -1],
       WEST: [-1, 0],
     };
   }
-  rotate() {}
-  report() {}
+
+  rotate(direction) {
+    const dirIndex = directions.indexOf(direction);
+    return {
+      LEFT: directions.at(
+        (dirIndex - 1 + directions.length) % directions.length
+      ),
+      RIGHT: directions.at((dirIndex + 1) % directions.length),
+    };
+  }
+
+  report(...details) {
+    const [name, command, x, y, dir] = details;
+    console.log(`${name} ${command} to position (${x}, ${y}) facing ${dir}.`);
+  }
 }
 
-const output = function (...details) {
-  const [name, command, x, y, dir] = details;
-  console.log(`${name} ${command} to position (${x}, ${y}) facing ${dir}.`);
-};
-
-let command;
+//Class to create a toy object that can do the following commands
 class Toy {
   //Constructor method to create the toy object and place it on the board.
   constructor(name, xPos, yPos, direction) {
@@ -36,45 +46,45 @@ class Toy {
     this.xPos = xPos; //x-direction
     this.yPos = yPos; //y-direction
     this.direction = direction; //direction object is facing
-
-    this.moveDirections = {
-      NORTH: [0, 1],
-      EAST: [1, 0],
-      SOUTH: [0, -1],
-      WEST: [-1, 0],
-    };
+    this.command = "";
+    this.getCommands = new Commands(); //Create an instance of Commands class.
   }
 
   place() {
-    command = "placed";
+    this.command = "placed";
     this.report();
   }
 
   move() {
-    command = "moved";
-    const [xDir, yDir] = this.moveDirections[this.direction] || [0, 0];
+    this.command = "moved";
+
+    //Get move directions from the move method
+    const moveDirections = this.getCommands.move();
+
+    const [xDir, yDir] = moveDirections[this.direction] || [0, 0];
     this.xPos += xDir;
     this.yPos += yDir;
     this.report();
   }
 
   rotate(side) {
-    this.side = side.toLowerCase();
-    command = `rotated ${this.side}`;
-    console.log(command);
-    //get current index of direction toy is facing.
-    const dirIndex = directions.indexOf(this.direction);
+    this.side = side.toUpperCase();
+    this.command = `rotated ${this.side}`;
 
-    //new direction is the next direction and if it is the end of the list return the mod.
-    this.direction =
-      this.side === "left"
-        ? directions.at((dirIndex - 1) % directions.length)
-        : directions.at((dirIndex + 1) % directions.length);
+    //Get rotate directions from the rotate method.
+    const rotateDirection = this.getCommands.rotate(this.direction);
+    this.direction = rotateDirection[this.side];
     this.report();
   }
 
   report() {
-    output(this.name, command, this.xPos, this.yPos, this.direction);
+    this.getCommands.report(
+      this.name,
+      this.command,
+      this.xPos,
+      this.yPos,
+      this.direction
+    );
   }
 }
 
